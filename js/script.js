0be539d05b0e39d22850d3e824e90e8a4fc7ad36
titopope@hotmail.com <titopope@hotmail.com>
2023-11-14 22:01:53 +0100

    ejercicio resuelto en clase

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
 import peliculas from './peliculas.js'
+console.log(peliculas)
+//genre_ids: genero
+//poster_path: imagenes
+//title: title
+const mostrarPeliculaPorGenero = (generoId, contenedorId) => {
+  const contenedor = document.getElementById(contenedorId)
+  const peliculaGenero = peliculas.filter((pelicula) => pelicula.genre_ids.includes(generoId))
+
+  peliculaGenero.forEach(pelicula => {
+    let templatePelis =`
+    <div class="pelicula">
+      <img src="https://image.tmdb.org/t/p/w200/${pelicula.backdrop_path}" alt="${pelicula.title}">
+      <h3>${pelicula.title}</h3>
+    </div>
+    `
+    contenedor.innerHTML += templatePelis
+  })
+
+}
+
+
+mostrarPeliculaPorGenero(28, "genero-28")
+mostrarPeliculaPorGenero(53, "genero-53")
+mostrarPeliculaPorGenero(12, "genero-12")
+
+
+
+
+
+
+
+
+
+/*este es con lo que tengo que seguir
+
 
 const accionDiv = document.getElementById('genero-28');
 const thrillerDiv = document.getElementById('genero-53');
@@ -10,7 +45,16 @@ const peliculasFiltradas = peliculas.map((pelicula) => {
     return pelicula.genre_ids.filter(28)
     
 });
-console.log(peliculasFiltradas);
+console.log(peliculasFiltradas);*/
+
+
+
+
+
+
+
+
+
 
 
 /*const accion = document.querySelectorAll('.genero-container')[0];
@@ -25,22 +69,13 @@ const peliculasAccion = peliculas.filter(generoAccion);
 console.log(peliculasAccion);
 */
 
-
-
-
-
-
 /*const generoAccion = element => element.gener_ids =28;
 const peliculasAccion = peliculas.some(generoAccion);
 const accionDos = peliculas.filter(generoAccion)
 console.log(peliculasAccion);
 console.log(accionDos);*/
 
-
-
 //const thriller = peliculas.filter((element) => element.genre_ids = 53);
-
-
 
 /*for (let i = 0; i < peliculas.length; i++) {
     if (peliculas[i].genre_ids === 28) {
@@ -56,8 +91,6 @@ console.log(accionDos);*/
         } 
     }
 */
-
-
 
 /*const accionDos = peliculas.filter((peliculas) => peliculas.genre_ids === 28)
 const thrillerDos = peliculas.filter((peliculas) => peliculas.genre_ids === 53);
